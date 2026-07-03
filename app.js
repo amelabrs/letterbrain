5c801411ab598b9f8bd92017816a7e3af1991c73
Amel <amel@Amels-MacBook-Air.local>
2026-07-03 15:34:23 +0530

    Revert: Case test levels back to mixed directions

--- app.js
+++ app.js
@@ -1081,7 +1081,7 @@ function startCapsGame(capsLevelIdx) {
         levelItems = [...ALL_ITEMS]; // full alphabet for distractor pool
         queue = shuffle([
             ...cumItems.map(item => ({ ...item, capsDirection: "caps-normal" })),
-            ...cumItems.map(item => ({ ...item, capsDirection: "caps-normal" }))
+            ...cumItems.map(item => ({ ...item, capsDirection: "caps-reverse" }))
         ]);
     } else {
         const targetItems = gl.letters.map(l => ALL_ITEMS.find(it => it.letter === l));
